8d75bc1e50d7ff82ef11589d11363e79232aed91
nmarum <nick.marum@gmail.com>
2020-07-02 22:45:10 -0400

    Loading previous analysis

---
+++ DND_approp_and_expenditures_since_2011.R
@@ -0,0 +1,88 @@
+#Exploring Main Estimates and expenditures and authorities data
+
+library(tidyverse)
+library(gtools)
+library(ggthemes)
+library(rvest)
+options(scipen = 999)
+
+#exploring Main Estimates and expenditures and authorities by Vote
+url <- "https://open.canada.ca/data/dataset/a35cf382-690c-4221-a971-cf0fd189a46f/resource/3bafde71-8cb8-460e-93e2-691295221063/download/eav_eac_en.csv"
+
+download.file(url, "eav_eac_en.csv")
+
+eav_eac_en <- read_csv("eav_eac_en.csv")
+head(eav_eac_en)
+dnd_eav_eac <- eav_eac_en %>% filter(org_name == "Department of National Defence")
+dnd_eav_eac
+dnd_eav_eac <- data.frame(dnd_eav_eac)
+class(dnd_eav_eac)
+
+dnd_eav_inmillions <- dnd_eav_eac %>% mutate(authorities_in_mill = (authorities/1000000), expenditures_in_mill = (expenditures/1000000))
+head(dnd_eav_inmillions)
+
+dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_and_statutory == "5") %>%
+  ggplot(aes(x=fy_ef)) +
+  geom_col(aes(y=expenditures_in_mill)) +
+  geom_line(aes(y=authorities_in_mill)) +
+  ggtitle("DND Vote 5 Appropriations and Spend ($millions)")+
+  theme_economist()
+  
+
+dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_and_statutory == "1") %>%
+  ggplot(aes(x=fy_ef)) +
+  geom_col(aes(y=expenditures_in_mill)) +
+  geom_line(aes(y=authorities_in_mill)) +
+  ggtitle("DND Vote 1 Appropriations and Spend ($millions)")+
+theme_economist()
+
+
+#percentage of spend against appropriated dollars
+dnd_eav_inmillions <- dnd_eav_inmillions %>% mutate(spend_percent = round(expenditures/authorities, 2))
+
+dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_and_statutory == "1") %>%
+  ggplot(aes(x=fy_ef, y=spend_percent, label=spend_percent)) +
+  geom_point() +
+  geom_label() +
+  ggtitle("DND Vote 1 - Proportion of authorities spent") +
+  theme_minimal()
+
+dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_and_statutory == "5") %>%
+  ggplot(aes(x=fy_ef, y=spend_percent, label=spend_percent)) +
+  geom_point() +
+  geom_label() +
+  ggtitle("DND Vote 5 - Proportion of authorities spent")+
+  theme_minimal()
+
+
+#DND expenditure by Standard Object
+
+url2 <- "https://open.canada.ca/data/dataset/a35cf382-690c-4221-a971-cf0fd189a46f/resource/27e54a33-3c39-42a9-8d58-46dd37c527e5/download/eso_eac_en.csv"
+
+download.file(url2, "easo.csv")
+
+easo_en <- read_csv("easo.csv")
+head(easo_en)
+
+dnd_easo <- easo_en %>% filter(org_name == "Department of National Defence")
+head(dnd_easo)
+class(dnd_easo)
+
+dnd_easo <- data.frame(dnd_easo)
+class(dnd_easo)
+nrow(dnd_easo)
+range(dnd_easo$expenditures)
+range(dnd_easo$fy_ef)
+
+dnd_easo %>% group_by(fy_ef) %>% summarize(sum(expenditures)) 
+#around 20B per year in total spending
+
+dnd_easo %>% group_by(sobj_en) %>% summarise(avg_per_year = sum(expenditures/(2018-2011)))
+#shows avg of  standard object spend since 2011.  Personnel clearly number one
+
+
+#separating out the defence procurement related items - as much as is possible.
+dnd_easo %>% filter(sobj_en %in% c("Acquisition of machinery and equipment", "Repair and maintenance", "Rentals")) %>%
+  ggplot(aes(x=fy_ef, y=expenditures, col=sobj_en))+
+  geom_point()
+
---
+++ import_wrangling_public_accounts_amo.R
@@ -0,0 +1,59 @@
+#Public Accts detailed information on acq of machinary and equipment
+#data wrangling
+library(tidyverse)
+
+
+url_2019 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2019-eng.csv"
+url_2018 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2018-eng.csv"
+url_2017 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2017-eng.csv"
+url_2016 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2016-eng.csv"
+url_2015 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2015-eng.csv"
+url_2014 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2014-eng.csv"
+url_2013 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2013-eng.csv"
+url_2012 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2012-eng.csv"
+url_2011 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2011-eng.csv"
+url_2010 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2010-eng.csv"
+
+
+
+download.file(url_2019, "amo_2019.csv")
+download.file(url_2018, "amo_2018.csv")
+download.file(url_2017, "amo_2017.csv")
+download.file(url_2016, "amo_2016.csv")
+download.file(url_2015, "amo_2015.csv")
+download.file(url_2014, "amo_2014.csv")
+download.file(url_2013, "amo_2013.csv")
+download.file(url_2012, "amo_2012.csv")
+download.file(url_2011, "amo_2011.csv")
+download.file(url_2010, "amo_2010.csv")
+
+
+amo_dnd_2019 <- read_csv("amo_2019.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2018 <- read_csv("amo_2018.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2017 <- read_csv("amo_2017.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2016 <- read_csv("amo_2016.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2015 <- read_csv("amo_2015.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2014 <- read_csv("amo_2014.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2013 <- read_csv("amo_2013.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2012 <- read_csv("amo_2012.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2011 <- read_csv("amo_2011.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+amo_dnd_2010 <- read_csv("amo_2010.csv") %>% filter(DEPT_EN_DESC == "National Defence")
+head(amo_dnd_2019)
+head(amo_dnd_2010)#checking to see if column names match throughout - all good!
+
+amo_2010to2019 <- bind_rows(amo_dnd_2010, amo_dnd_2011)
+amo_2010to2019 <- bind_rows(amo_2010to2019, amo_dnd_2012)
+amo_2010to2019 <- bind_rows(amo_2010to2019, amo_dnd_2013)
+amo_2010to2019 <- bind_rows(amo_2010to2019, amo_dnd_2014)
+amo_2010to2019 <- bind_rows(amo_2010to2019, amo_dnd_2015)
+amo_2010to2019 <- bind_rows(amo_2010to2019, amo_dnd_2016)
+amo_2010to2019 <- bind_rows(amo_2010to2019, amo_dnd_2017)
+amo_2010to2019 <- bind_rows(amo_2010to2019, amo_dnd_2018)
+amo_2010to2019 <- bind_rows(amo_2010to2019, amo_dnd_2019)
+
+save(amo_2010to2019, file = "amo_2010to2019.rda")#create RDA file containing the data
+file.exists("amo_2010to2019.rda") #check to make sure it was successful
+view(amo_2010to2019)
+
+file.remove(c("amo_2019.csv", "amo_2018.csv", "amo_2017.csv", "amo_2016.csv", "amo_2015.csv", "amo_2014.csv", "amo_2013.csv", "amo_2012.csv", "amo_2011.csv", "amo_2010.csv"))
+#clean up complete!
