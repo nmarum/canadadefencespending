0b6c3d3a3faabbbca66672d719e402a26f6344f5
nmarum <nick.marum@gmail.com>
2020-07-05 19:31:59 -0400

    short script for importing vendor name data and transforming to .rda.

---
+++ import_vendor_name_data.R
@@ -0,0 +1,16 @@
+#import of vendor name data from GoC-Spending on Github
+
+#GoC vendor names grouped into "parent companies".  Many unique vendor names were
+#data entry errors so having this as ref point should help improve analysis quality.
+
+#Planning to use this vendor name data to update analysis of contract data.
+
+library(tidyverse)
+library(rvest)
+
+url <- "https://raw.githubusercontent.com/GoC-Spending/goc-spending-vendors/master/vendor_data.csv"
+
+download.file(url, "vendor_data.csv")
+vendor_data <- read.csv("vendor_data.csv")
+view(vendor_data)
+save(vendor_data, file ="vendor_data.rda")
