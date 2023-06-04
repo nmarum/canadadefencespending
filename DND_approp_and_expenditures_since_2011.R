38b473becff87c5b643ec0dae7221ac2b9622d3b
nmarum <nick.marum@gmail.com>
2023-06-03 20:47:52 -0400

    adding new pipe

--- DND_approp_and_expenditures_since_2011.R
+++ DND_approp_and_expenditures_since_2011.R
@@ -17,11 +17,17 @@ dnd_eav_eac
 dnd_eav_eac <- data.frame(dnd_eav_eac)
 class(dnd_eav_eac)
 
-dnd_eav_inmillions <- dnd_eav_eac %>% mutate(authorities_in_mill = as.numeric(authorities)/1000000,
-                                             expenditures_in_mill = as.numeric(expenditures)/1000000)
+dnd_eav_inmillions <- dnd_eav_eac |>
+  mutate(authorities_in_mill = as.numeric(authorities)/1000000,
+         expenditures_in_mill = as.numeric(expenditures)/1000000)
+
 head(dnd_eav_inmillions)
 
-dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(authorities_in_mill), !is.na(fy_ef), vote_and_statutory == "5") %>%
+dnd_eav_inmillions |>
+  filter(!is.na(expenditures_in_mill),
+         !is.na(authorities_in_mill),
+         !is.na(fy_ef),
+         vote_and_statutory == "5") |>
   ggplot(aes(x=fy_ef)) +
   geom_point(aes(y=authorities_in_mill), size=3, col="red") +
   geom_col(aes(y=expenditures_in_mill)) +
@@ -32,7 +38,10 @@ dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(authorities_i
   theme_minimal()
   
 
-dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_and_statutory == "1") %>%
+dnd_eav_inmillions |>
+  filter(!is.na(expenditures_in_mill),
+         !is.na(fy_ef),
+         vote_and_statutory == "1") |>
   ggplot(aes(x=fy_ef)) +
   geom_point(aes(y=authorities_in_mill), size=3, col="red") +
   geom_col(aes(y=expenditures_in_mill)) +
@@ -40,13 +49,16 @@ dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_
   ylab("$ in Millions")+
   scale_y_continuous() +
   ggtitle("DND Vote 1 Appropriations and Spend ($millions)")+
-theme_minimal()
+  theme_minimal()
 
 
 #percentage of spend against appropriated dollars
-dnd_eav_inmillions <- dnd_eav_inmillions %>% mutate(spend_percent = round(as.numeric(expenditures)/as.numeric(authorities), 2))
+dnd_eav_inmillions <- dnd_eav_inmillions |>
+  mutate(spend_percent = round(as.numeric(expenditures)/as.numeric(authorities), 2))
 
-dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_and_statutory == "1") %>%
+dnd_eav_inmillions |>
+  filter(!is.na(expenditures_in_mill),
+         !is.na(fy_ef), vote_and_statutory == "1") |>
   ggplot(aes(x=fy_ef, y=spend_percent, label=spend_percent*100)) +
   geom_point() +
   geom_label() +
@@ -55,7 +67,9 @@ dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_
   ggtitle("DND Vote 1 - Proportion of authorities spent") +
   theme_minimal()
 
-dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_and_statutory == "5") %>%
+dnd_eav_inmillions |>
+  filter(!is.na(expenditures_in_mill),
+         !is.na(fy_ef), vote_and_statutory == "5") |>
   ggplot(aes(x=fy_ef, y=spend_percent, label=spend_percent*100)) +
   geom_point() +
   geom_label() +
@@ -70,7 +84,9 @@ dnd_eav_inmillions %>% filter(!is.na(expenditures_in_mill), !is.na(fy_ef), vote_
 easo_en <- read_csv("https://open.canada.ca/data/dataset/a35cf382-690c-4221-a971-cf0fd189a46f/resource/27e54a33-3c39-42a9-8d58-46dd37c527e5/download/eso_eac_en.csv")
 head(easo_en)
 
-dnd_easo <- easo_en %>% filter(org_name == "Department of National Defence")
+dnd_easo <- easo_en |>
+  filter(org_name == "Department of National Defence")
+
 head(dnd_easo)
 class(dnd_easo)
 
@@ -80,18 +96,20 @@ nrow(dnd_easo)
 range(dnd_easo$expenditures)
 range(dnd_easo$fy_ef)
 
-dnd_easo %>% group_by(fy_ef) %>%
+dnd_easo %>% group_by(fy_ef) |>
   summarize(total_spend = sum(as.numeric(expenditures))) 
 #around 20B per year in total spending
 
-dnd_easo %>% group_by(sobj_en) %>%
-  summarise(avg_per_year = sum(as.numeric(expenditures)/length(unique(fy_ef)))) %>%
+dnd_easo %>% group_by(sobj_en) |>
+  summarise(avg_per_year = sum(as.numeric(expenditures)/length(unique(fy_ef)))) |>
   arrange(desc(avg_per_year))
 #shows avg of  standard object spend since 2011.  Personnel clearly number one
 
 
 #separating out the defence procurement related items - as much as is possible.
-dnd_easo %>% filter(sobj_en %in% c("Acquisition of machinery and equipment", "Repair and maintenance", "Rentals")) %>%
+dnd_easo |>
+  filter(sobj_en %in% c("Acquisition of machinery and equipment",
+                        "Repair and maintenance", "Rentals")) |>
   ggplot(aes(x=as.character(fy_ef), y=expenditures, col=sobj_en))+
   geom_point(size=4) +
   xlab("FY") +
