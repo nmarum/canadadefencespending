985ad496cf26ce78b1a440028055b1bed4be188c
nmarum <nick.marum@gmail.com>
2024-06-01 20:00:17 -0400

    update for 2023 data

--- import_wrangling_public_accounts_amo.R
+++ import_wrangling_public_accounts_amo.R
@@ -2,6 +2,7 @@
 #data wrangling
 library(tidyverse)
 
+url_2023 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2023.csv"
 url_2022 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2022.csv"
 url_2021 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2021-eng.csv"
 url_2020 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2020-eng.csv"
@@ -16,6 +17,7 @@ url_2012 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2012-eng.
 url_2011 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2011-eng.csv"
 url_2010 <- "http://donnees-data.tpsgc-pwgsc.gc.ca/ba1/amo-ame/amo-ame-2010-eng.csv"
 
+download.file(url_2022, "amo_2023.csv")
 download.file(url_2022, "amo_2022.csv")
 download.file(url_2021, "amo_2021.csv")
 download.file(url_2020, "amo_2020.csv")
@@ -30,6 +32,7 @@ download.file(url_2012, "amo_2012.csv")
 download.file(url_2011, "amo_2011.csv")
 download.file(url_2010, "amo_2010.csv")
 
+amo_dnd_2023 <- read_csv("amo_2023.csv") |> filter(DEPT_EN_DESC == "National Defence (Department of)")
 amo_dnd_2022 <- read_csv("amo_2022.csv") |> filter(DEPT_EN_DESC == "National Defence (Department of)")
 amo_dnd_2021 <- read_csv("amo_2021.csv") |> filter(DEPT_EN_DESC == "National Defence (Department of)")
 amo_dnd_2020 <- read_csv("amo_2020.csv") |> filter(DEPT_EN_DESC == "National Defence (Department of)")
@@ -58,10 +61,11 @@ amo_2010to2020 <- bind_rows(amo_2010to2020, amo_dnd_2019)
 amo_2010to2020 <- bind_rows(amo_2010to2020, amo_dnd_2020)
 amo_2010to2020 <- bind_rows(amo_2010to2020, amo_dnd_2021)
 amo_2010to2020 <- bind_rows(amo_2010to2020, amo_dnd_2022)
+amo_2010to2020 <- bind_rows(amo_2010to2020, amo_dnd_2023)
 
 save(amo_2010to2020, file = "amo_2010to2020.rda")#create RDA file containing the data
 file.exists("amo_2010to2020.rda") #check to make sure it was successful
 view(amo_2010to2020)
 
-file.remove(c("amo_2022.csv", "amo_2021.csv", "amo_2020.csv", "amo_2019.csv", "amo_2018.csv", "amo_2017.csv", "amo_2016.csv", "amo_2015.csv", "amo_2014.csv", "amo_2013.csv", "amo_2012.csv", "amo_2011.csv", "amo_2010.csv"))
+file.remove(c("amo_2023.csv", "amo_2022.csv", "amo_2021.csv", "amo_2020.csv", "amo_2019.csv", "amo_2018.csv", "amo_2017.csv", "amo_2016.csv", "amo_2015.csv", "amo_2014.csv", "amo_2013.csv", "amo_2012.csv", "amo_2011.csv", "amo_2010.csv"))
 #clean up complete!
